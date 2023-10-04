75de7cb479ea3431473327e9b59e74971cea55c8
HissahKh <146759947+HissahKh@users.noreply.github.com>
2023-10-04 10:29:23 +0300

    Update Node.java

--- Csc212-project/src/Csc212Project/Node.java
+++ Csc212-project/src/Csc212Project/Node.java
@@ -2,6 +2,38 @@ package Csc212Project;
 
 public class Node {
 
-  //hi
+  private T data;
+	private Node<T> next;
+	
+	
+	public Node() {
+		data = null;
+		next = null;
+	}
+	
+	public Node(T data) {
+		
+		this.data = data;
+		next = null;
+		
+	}
+
+  public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public Node<T> getNext() {
+		return next;
+	}
+
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+	
+
 
 }
