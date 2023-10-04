fba2023854c0affa0f5feef7ca71e40f0918790b
HissahKh <146759947+HissahKh@users.noreply.github.com>
2023-10-04 10:14:22 +0300

    Update LinkedList.java

--- Csc212-project/src/Csc212Project/LinkedList.java
+++ Csc212-project/src/Csc212Project/LinkedList.java
@@ -1,6 +1,118 @@
 package Csc212Project;
 
-public class LinkedList {
+public class LinkedList<T>{
+
+  private Node<T> head;
+	
+	
+	
+	// needs modifying 
+	
+
+	public LinkedList() {
+		
+		head = null;
+		
+	}
+
+	public LinkedList(Node<T> head) {
+		
+		head = head;
+		
+	}
+	
+	public boolean isEmpty() {
+		
+		return head == null;
+		
+	}
+	
+	public void add(T n) {
+		
+		Node<T> newNode = new Node<>(n);
+		
+		if(isEmpty())
+			head = newNode;
+		
+		else {
+			
+			Node<T> current = head;
+			
+			while(current.getNext() != null) {
+				
+				current = current.getNext();
+				
+			}
+			
+			current.setNext(newNode);		
+			
+		}	
+		
+	}
+	
+	public T search(T s) {
+		
+		if(isEmpty())
+			return null; //message?
+		
+		else {
+			
+			Node<T> current = head;
+			
+			while (current != null) {
+				
+				if(current.getData().equals(s))
+					return current.getData();
+				
+				current = current.getNext();
+				
+			}
+			
+			return null;
+			
+		}
+		
+	}
+	
+	public void delete(T d) {
+		
+		if(isEmpty())
+			return;   //message?
+		
+		if(head.getData().equals(d)) {
+			head = head.getNext();
+			return;
+		}
+		
+		else {
+			
+            Node<T> current = head.getNext();
+            Node<T> previous = head;
+            	
+			while (current != null) {
+				
+				if(current.getData().equals(d))
+					previous.setNext(current.getNext());
+				
+				else {
+					
+					previous = current;
+					
+					current = current.getNext();
+		
+				}
+				
+			}
+					
+		}
+	
+	
+	
+	
+	
+
+}
+
 
   
 
