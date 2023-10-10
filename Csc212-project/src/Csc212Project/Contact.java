db6683840d8c89d555faa4d500ade41128dad451
Tarimaf7 <146776043+Tarimaf7@users.noreply.github.com>
2023-10-10 11:30:59 +0300

    Update Contact.java

--- Csc212-project/src/Csc212Project/Contact.java
+++ Csc212-project/src/Csc212Project/Contact.java
@@ -1,12 +1,15 @@
-package Csc212Project;
+package com.mycompany.phonebook;
 
-public class Contact implements Comparable<String>{
+
+public class Contact implements Comparable<String> {
+    
 private String contactName;
 private String phoneNumber;
 private String emailAddress;
 private String address;
 private String birthday;
 private String notes;
+private LinkedList<Event> events;
 
 public Contact(){
 }
@@ -18,20 +21,31 @@ emailAddress=c.emailAddress;
 address=c.address;
 birthday=c.birthday;
 notes=c.notes;
+events=c.events;
 }
 
-public Contact(String contactName, String phoneNumber, String emailAddress, String address, String birthday, String notes){
+public Contact(String contactName, String phoneNumber, String emailAddress, String address, String birthday, String notes, LinkedList<Event> events){
 this.contactName=contactName;
 this.phoneNumber=phoneNumber;
 this.emailAddress=emailAddress;
 this.address=address;
 this.birthday=birthday;
 this.notes=notes;
+this.events=events;
 }
 
 public String getContactName(){
 return contactName;
 }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public LinkedList<Event> getEvents() {
+        return events;
+    }
+
 
 public void setContactName(String n){
 contactName=n;
@@ -40,10 +54,6 @@ contactName=n;
 public void setPhoneNumber(String p){
 phoneNumber=p;
 }
-  public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
 
 public void setEmailAddress(String e){
 emailAddress=e;
@@ -61,10 +71,12 @@ public void setNotes(String no){
 notes=no;
 }
 
+
 public void display(){
 System.out.print("contact name: "+contactName+" phone number: "+phoneNumber+" email address: "+emailAddress+" address: "+address+" birthday: "+birthday+" notes:"+notes);
 }
 
+@Override
 public int compareTo(String s){
 return contactName.compareTo(s);
 }
@@ -77,5 +89,3 @@ return contactName.compareTo(c.contactName);
 
 
 }
-
-
