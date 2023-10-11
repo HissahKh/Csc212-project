bb4c9498b7833a23d9d46255b78b8d15d12e6f39
HissahKh <146759947+HissahKh@users.noreply.github.com>
2023-10-11 19:51:06 +0300

    Update LinkedList.java

--- Csc212-project/src/Csc212Project/LinkedList.java
+++ Csc212-project/src/Csc212Project/LinkedList.java
@@ -31,6 +31,146 @@ public class LinkedList<T>{
 		
 		Node<T> newNode = new Node<>(n);
 		
+		if(isEmpty()) {
+			head = newNode;
+			return;
+		}
+		
+		if(exist((Contact) n)) {
+			
+			return;
+			
+		}
+		
+		if(((Contact) newNode.getData()).getContactName().compareToIgnoreCase(((Contact) head.getData()).getContactName()) < 0) {
+			
+			newNode.setNext(head);
+			
+			head = newNode;
+			
+			return;
+			
+		}
+	
+		else {
+			
+			Node<T> current = head;
+			
+			while(current != null) {
+				
+				if(((Contact) newNode.getData()).compareTo(((Contact) current.getData())) < 0) {
+					
+					current = current.getNext();
+					
+					continue;
+					
+				}				
+				
+				Node<T> temp = current.getNext();
+				
+				current.setNext(newNode);
+				
+				newNode.setNext(temp);
+				
+			}
+			
+			current.setNext(newNode);		
+			
+		}	
+		
+	}
+	
+	/*public T search(T s, int n) {
+		
+		if(isEmpty())
+			return null; //message?
+		
+		if(n == 1) {
+			
+			
+			
+		}
+		
+		else {
+			
+			Node<T> current = head;
+			
+			while (current != null) {
+				
+				if(current.getData().equals(s))
+					return current.getData();
+				
+				current = current.getNext();
+				
+			}
+			
+			return null;
+			
+		}
+		
+	}*/
+	
+	public void delete(T d) {
+		
+		if(isEmpty())
+			return;   //message?
+		
+		if(head.getData().equals(d)) {
+			head = head.getNext();
+			return;
+		}
+		
+		else {
+			
+            Node<T> current = head.getNext();
+            Node<T> previous = head;
+            	
+			while (current != null) {
+				
+				if(current.getData().equals(d))
+					previous.setNext(current.getNext());
+				
+				else {
+					
+					previous = current;
+					
+					current = current.getNext();
+		
+				}
+				
+			}
+					
+		}
+	
+	
+	
+	
+	
+
+}
+	
+	public boolean exist(Contact c) {
+		
+		Node<Contact> current = (Node<Contact>) head;
+		
+		while(current != null){
+			
+			if(current.getData().getContactName().compareTo(c.getContactName()) == 0 || current.getData().getPhoneNumber().compareTo(c.getPhoneNumber()) == 0)
+			   return true;
+				
+			current = current.getNext();
+			
+		}
+		
+		return false;
+	}
+
+  
+
+}
+
+		Node<T> newNode = new Node<>(n);
+		
 		if(isEmpty())
 			head = newNode;
 		
